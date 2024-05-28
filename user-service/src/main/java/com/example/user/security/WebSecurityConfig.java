007754c0a68dc6b40a54add245b35911aa5aea7a
Filip Hadzi-Ristic <filip.hadzi.ristic@msg-global.com>
2024-05-28 21:54:02 +0200

    User-Service: Updated WebSecurityConfig

--- user-service/src/main/java/com/example/user/security/WebSecurityConfig.java
+++ user-service/src/main/java/com/example/user/security/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.user.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
@@ -12,6 +16,13 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class WebSecurityConfig
 {
+	private JwtTokenProvider jwtTokenProvider;
+
+	@Autowired
+	public WebSecurityConfig(JwtTokenProvider jwtTokenProvider)
+	{
+		this.jwtTokenProvider = jwtTokenProvider;
+	}
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
@@ -22,8 +33,16 @@ public class WebSecurityConfig
 						.requestMatchers(new AntPathRequestMatcher("/users/register"), new AntPathRequestMatcher("/users/login"))
 						.permitAll()
 						.anyRequest().authenticated()
+						.and()
+						.addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
 				);
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
 	}
 }
 
